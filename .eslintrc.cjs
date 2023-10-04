8e8749f28229e44404fe9a661be2e0a37fe38784
jhchoi1182 <jhchoi1182@gmail.com>
2023-10-04 20:16:31 +0900

    [CHORE] 프로젝트 초기 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "import"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "external",
+          "internal",
+          ["parent", "sibling", "index"],
+          "object",
+        ],
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+  },
+};
