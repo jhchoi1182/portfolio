8b3bb09561c9d3e9e6ba6d22f0b36f90188b381b
jhchoi1182 <jhchoi1182@gmail.com>
2023-10-18 15:08:30 +0900

    [REFACTOR] overview 리팩토링

--- src/components/mainBanner/elements/DeepDive.tsx
+++ src/components/mainBanner/elements/DeepDive.tsx
@@ -5,6 +5,12 @@ interface props {
   refs: React.MutableRefObject<HTMLDivElement | null>[];
 }
 
+const texts = [
+  `궁금한 부분을 파고들 때 흥미를 느낍니다.`,
+  `웹 개발에 있어 물음표는 저에게 "도전"을 의미합니다.`,
+  `로직이나 문제가 복잡하다면, 오히려 더 깊게 파고들 수 있는 기회라고 생각합니다.`,
+];
+
 function DeepDive({ showElements, refs }: props) {
   return (
     <div className="relative flex flex-col items-center gap-5 z-20">
@@ -16,25 +22,15 @@ function DeepDive({ showElements, refs }: props) {
       >
         DeepDive
       </div>
-      <div
-        ref={refs[1]}
-        className={`fadeInUpAfter450ms ${showElements[1] ? "show" : ""}`}
-      >
-        궁금한 부분을 파고들 때 흥미를 느낍니다.
-      </div>
-      <div
-        ref={refs[1]}
-        className={`fadeInUpAfter450ms ${showElements[1] ? "show" : ""}`}
-      >
-        웹 개발에 있어 물음표는 저에게 "도전"을 의미합니다.
-      </div>
-      <div
-        ref={refs[1]}
-        className={`fadeInUpAfter450ms ${showElements[1] ? "show" : ""}`}
-      >
-        로직이나 문제가 복잡하다면, 오히려 더 깊게 파고들 수 있는 기회라고
-        생각합니다.
-      </div>
+      {texts.map((text, i) => (
+        <div
+          key={i}
+          ref={refs[1]}
+          className={`fadeInUpAfter450ms ${showElements[1] ? "show" : ""}`}
+        >
+          {text}
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/mainBanner/elements/Perseverance.tsx
+++ src/components/mainBanner/elements/Perseverance.tsx
@@ -5,6 +5,12 @@ interface props {
   refs: React.MutableRefObject<HTMLDivElement | null>[];
 }
 
+const texts = [
+  `문제가 어렵더라도, 어떤 벽에 막히더라도 쉽게 포기하지 않습니다.`,
+  `주어진 문제를 해결할 때까지 꾸준히 나아가며,`,
+  `이 과정에서 배운 것들을 기록하는 습관이 있습니다.`,
+];
+
 function Perseverance({ showElements, refs }: props) {
   return (
     <div className="relative flex flex-col items-center gap-5 mt-40 z-20">
@@ -16,24 +22,15 @@ function Perseverance({ showElements, refs }: props) {
       >
         Perseverance
       </div>
-      <div
-        ref={refs[3]}
-        className={`fadeInUpAfter850ms ${showElements[3] ? "show" : ""}`}
-      >
-        문제가 어렵더라도, 어떤 벽에 막히더라도 쉽게 포기하지 않습니다.
-      </div>
-      <div
-        ref={refs[3]}
-        className={`fadeInUpAfter850ms ${showElements[3] ? "show" : ""}`}
-      >
-        주어진 문제를 해결할 때까지 꾸준히 나아가며,
-      </div>
-      <div
-        ref={refs[3]}
-        className={`fadeInUpAfter850ms ${showElements[3] ? "show" : ""}`}
-      >
-        이 과정에서 배운 것들을 기록하는 습관이 있습니다.
-      </div>
+      {texts.map((text, i) => (
+        <div
+          key={i}
+          ref={refs[3]}
+          className={`fadeInUpAfter450ms ${showElements[3] ? "show" : ""}`}
+        >
+          {text}
+        </div>
+      ))}
     </div>
   );
 }
