0a5e15239551e002bd8b59d1caf28c43da8ea28c
jhchoi1182 <jhchoi1182@gmail.com>
2023-10-20 22:56:38 +0900

    [FIX] 심장박동 로직, 애니메이션 수정

--- src/components/mainBanner/elements/HeartBeat.tsx
+++ src/components/mainBanner/elements/HeartBeat.tsx
@@ -1,17 +1,67 @@
 import { useRecoilValue } from "recoil";
-import heartBeatDark from "../../../assets/images/heartBeat_dark.webp";
-import heartBeatLight from "../../../assets/images/heartBeat_light.webp";
 import { darkModeAtom } from "../../../libs/atoms";
 
-const HeartBeat = () => {
+function HeartBeat() {
   const isDark = useRecoilValue(darkModeAtom);
 
   return (
-    <div className="absolute inset-0 w-full z-0 overflow-hidden">
-      <img src={isDark ? heartBeatDark : heartBeatLight} alt="심장박동" />
-      <div className="absolute top-0 w-[200px] h-full shadow-lightWindow dark:shadow-darkWindow heartbeat"></div>
-    </div>
+    <svg
+      className="absolute inset-0 w-full z-0 overflow-hidden heartbeat"
+      width="1919"
+      height="531"
+      viewBox="0 0 1919 531"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {isDark ? (
+        <>
+          <path
+            d="M0 323.805H980.555L1047.73 80.5745L1136.96 529L1244.74 2L1317.43 430.907L1381.6 219.206L1426.72 323.805H1919"
+            stroke="url(#paint0_radial_49_41)"
+            stroke-width="20"
+          />
+          <defs>
+            <radialGradient
+              id="paint0_radial_49_41"
+              cx="0"
+              cy="0"
+              r="1"
+              gradientUnits="userSpaceOnUse"
+              gradientTransform="translate(-0.501279 529.5) scale(1920 6968.26)"
+            >
+              <stop stop-color="#778DFF" />
+              <stop offset="0.536458" stop-color="#0D0B97" />
+              <stop offset="0.692708" stop-color="#0D0B97" />
+              <stop offset="1" stop-color="#778DFF" />
+            </radialGradient>
+          </defs>
+        </>
+      ) : (
+        <>
+          <path
+            d="M0 323.805H980.555L1047.73 80.5745L1136.96 529L1244.74 2L1317.43 430.907L1381.6 219.206L1426.72 323.805H1919"
+            stroke="url(#paint0_radial_12_7)"
+            stroke-width="20"
+          />
+          <defs>
+            <radialGradient
+              id="paint0_radial_12_7"
+              cx="0"
+              cy="0"
+              r="1"
+              gradientUnits="userSpaceOnUse"
+              gradientTransform="translate(-0.501279 529.5) scale(1920 6968.26)"
+            >
+              <stop stop-color="#107557" />
+              <stop offset="0.604945" stop-color="#70FFD4" />
+              <stop offset="0.679836" stop-color="#70FFD4" />
+              <stop offset="1" stop-color="#107557" />
+            </radialGradient>
+          </defs>
+        </>
+      )}
+    </svg>
   );
-};
+}
 
 export default HeartBeat;
