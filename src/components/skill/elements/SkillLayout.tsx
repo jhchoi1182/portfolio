c1c226255e2aec3b92dfe14d7f86b2d261220f92
jhchoi1182 <jhchoi1182@gmail.com>
2023-10-30 01:22:37 +0900

    [FEAT] Skill 반응형 작업

--- src/components/skill/elements/SkillLayout.tsx
+++ src/components/skill/elements/SkillLayout.tsx
@@ -7,17 +7,24 @@ interface Props {
 
 function SkillLayout({ title, li, showElement, divRef }: Props) {
   return (
-    <div
-      ref={divRef}
-      className={`flex fadeInUpAfter250ms ${showElement ? "show" : ""}`}
-    >
-      <div className="w-[290px] text-2xl font-semibold">{title}</div>
-      <ul className="ml-10 text-xl list-disc space-y-4">
-        {li.map((v, i) => (
-          <li key={i}>{v}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div
+        ref={divRef}
+        className={`block w-[400px] fadeInUpAfter250ms xl:w-[910px] lg:flex lg:w-[810px] md:w-[520px] ${
+          showElement ? "show" : ""
+        }`}
+      >
+        <div className="w-[290px] mb-5 text-xl font-semibold xl:text-2xl lg:mb-0">
+          {title}
+        </div>
+        <ul className="text-sm list-disc space-y-4 xl:text-xl xl:ml-10 md:text-lg">
+          {li.map((v, i) => (
+            <li key={i}>{v}</li>
+          ))}
+        </ul>
+      </div>
+      <hr className="block w-full h-[2px] -my-2 bg-gray-300 dark:bg-zinc-900 lg:hidden md:my-0" />
+    </>
   );
 }
 
--- src/components/skill/index/Skill.tsx
+++ src/components/skill/index/Skill.tsx
@@ -24,24 +24,24 @@ function Skill() {
   return (
     <div
       ref={containerRef}
-      className="relative flex flex-col items-center w-full h-screen font-notoSans z-20"
+      className="relative flex flex-col items-center w-full min-h-screen font-notoSans z-20"
     >
       <h1
         ref={refs[0]}
-        className={`text-4xl font-semibold mb-16 z-10 fadeInUpAfter250ms ${
+        className={`text-3xl font-semibold mb-16 z-10 fadeInUpAfter250ms md:text-4xl ${
           showElements[0] ? "show" : ""
         }`}
       >
         Skills
       </h1>
-      <div className="flex flex-col gap-12 z-10">
+      <div className="flex flex-col gap-12 w-full items-center z-10">
         <FrontEnd showElement={showElements[1]} divRef={refs[1]} />
         <Data showElement={showElements[2]} divRef={refs[2]} />
         <CloudAndDeployment showElement={showElements[3]} divRef={refs[3]} />
         <TestingLibraries showElement={showElements[4]} divRef={refs[4]} />
         <Collaboration showElement={showElements[5]} divRef={refs[5]} />
       </div>
-      <div className="absolute inset-0 w-full overflow-hidden z-0">
+      <div className="absolute top-1/2 left-0 -translate-y-1/2 w-full overflow-hidden z-0 lg:top-0 lg:left-0 lg:translate-y-0">
         <Curve containerRef={containerRef} />
       </div>
     </div>
