a8b61f16ed702df2bf26611980dc14c8c9ae1e12
jhchoi1182 <jhchoi1182@gmail.com>
2023-10-31 01:17:20 +0900

    [FIX] 프로젝트 내용 수정

--- src/components/mainBanner/elements/Developer.tsx
+++ src/components/mainBanner/elements/Developer.tsx
@@ -8,7 +8,7 @@ function Developer() {
         Developer
       </div>
       <a
-        href="https://whispering-keyboard-d37.notion.site/34e7062a268b40b49296376979618c6b?pvs=4"
+        href="https://www.canva.com/design/DAFyXmbskLU/_TTl-7q3qWzsXvieWG_siA/edit?utm_content=DAFyXmbskLU&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
         rel="noopener noreferrer"
         target="_blank"
         className="absolute top-[25px] -right-10 flex text-xs items-center revealTextAfter3250ms lg:top-24 md:top-[70px] md:-right-12 md:text-base sm:text-sm sm:top-[40px]"
