3b0dd2cf325630d03d7fb6904946ab6009f6f739
jhchoi1182 <jhchoi1182@gmail.com>
2023-11-06 17:42:19 +0900

    [REAFACTOR] 디테일 페이지 리팩토링

---
+++ src/components/detail/elements/ProjectLink.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { GoArrowUpRight } from "react-icons/go";
+import { center } from "../../../styles/classNames";
+
+function ProjectLink({ data }: { data: projectMetaData }) {
+  const [isHovered, setHoveredButton] = useState<string | null>(null);
+  const isTwoProject = data.title.split("&").length !== 1;
+
+  return (
+    <div className="flex gap-10">
+      {data.title.split("&").map((title, i) => (
+        <div
+          key={i}
+          className={`flex flex-col gap-6 ${
+            isTwoProject ? "items-center" : ""
+          }`}
+        >
+          {isTwoProject && <h5>{title}</h5>}
+          <a
+            className="text-4xl cursor-pointer"
+            href={data.github[i]}
+            rel="noreferrer"
+            target="_blank"
+          >
+            <button
+              className={`${center} w-14 h-14 border border-black rounded-full`}
+            >
+              <AiFillGithub className="w-6 h-6" />
+            </button>
+          </a>
+          <a href={data.service[i]} rel="noreferrer" target="_blank">
+            <button
+              className={`${center} gap-5 w-32 h-11 bg-inverse dark:bg-primary rounded-full`}
+              onMouseEnter={() => setHoveredButton(title)}
+              onMouseLeave={() => setHoveredButton(null)}
+            >
+              {isHovered === title ? (
+                <GoArrowUpRight className="w-5 h-5 -ml-1 -mr-1 text-white dark:text-black" />
+              ) : (
+                <div className="w-3 h-3 bg-white dark:bg-black rounded-full" />
+              )}
+              <span className="text-xl font-semibold text-white dark:text-black">
+                View
+              </span>
+            </button>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ProjectLink;
--- src/components/detail/index/MetaInfo.tsx
+++ src/components/detail/index/MetaInfo.tsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-import { AiFillGithub } from "react-icons/ai";
-import { GoArrowUpRight } from "react-icons/go";
 import { useParams } from "react-router-dom";
 import { projectMetaDatas } from "../../../assets/data/projectMetaData";
-import { center } from "../../../styles/classNames";
+import ProjectLink from "../elements/ProjectLink";
 
 function MetaInfo() {
-  const [isHovered, setHoveredButton] = useState<string | null>(null);
   const { id } = useParams();
   const data = projectMetaDatas[id ?? "portfolio"];
   const isTeamProject = data.projectType === "team";
-  const isTwoProject = data.title.split("&").length !== 1;
 
   return (
     <div className="flex flex-col justify-between w-full pt-[200px] font-notoSans lg:sticky lg:top-0 lg:w-1/2 lg:h-screen lg:pt-[15%]">
@@ -37,46 +32,7 @@ function MetaInfo() {
         <span className="text-[#636e72] dark:text-[#b2bec3]">{`'${
           data.date.start
         } - ${data.date.end !== "진행중" ? "'" : ""}${data.date.end}`}</span>
-        <div className="flex gap-10">
-          {data.title.split("&").map((title, i) => (
-            <div
-              key={i}
-              className={`flex flex-col gap-6 ${
-                isTwoProject ? "items-center" : ""
-              }`}
-            >
-              {isTwoProject && <h5>{title}</h5>}
-              <a
-                className="text-4xl cursor-pointer"
-                href={data.github[i]}
-                rel="noreferrer"
-                target="_blank"
-              >
-                <button
-                  className={`${center} w-14 h-14 border border-black rounded-full`}
-                >
-                  <AiFillGithub className="w-6 h-6" />
-                </button>
-              </a>
-              <a href={data.service[i]} rel="noreferrer" target="_blank">
-                <button
-                  className={`${center} gap-5 w-32 h-11 bg-inverse dark:bg-primary rounded-full`}
-                  onMouseEnter={() => setHoveredButton(title)}
-                  onMouseLeave={() => setHoveredButton(null)}
-                >
-                  {isHovered === title ? (
-                    <GoArrowUpRight className="w-5 h-5 -ml-1 -mr-1 text-white dark:text-black" />
-                  ) : (
-                    <div className="w-3 h-3 bg-white dark:bg-black rounded-full" />
-                  )}
-                  <span className="text-xl font-semibold text-white dark:text-black">
-                    View
-                  </span>
-                </button>
-              </a>
-            </div>
-          ))}
-        </div>
+        <ProjectLink data={data} />
       </div>
     </div>
   );
