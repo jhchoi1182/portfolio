07cd8355aab93825fa7d3429ccce6773628072d4
jhchoi1182 <jhchoi1182@gmail.com>
2024-01-24 23:15:04 +0900

    fix: 메인배너 글자 위치 재조정

--- src/components/mainBanner/elements/Developer.tsx
+++ src/components/mainBanner/elements/Developer.tsx
@@ -4,14 +4,14 @@ import { GoArrowUpRight } from "react-icons/go";
 function Developer() {
   return (
     <div className="relative flex z-20">
-      <div className="text-4xl animate-slideToLeftAfter3000ms lg:text-9xl md:text-8xl sm:text-6xl">
+      <div className="text-4xl animate-slideToLeftAfter2500ms lg:text-9xl md:text-8xl sm:text-6xl">
         Developer
       </div>
       <a
         href="https://www.canva.com/design/DAFyXmbskLU/_TTl-7q3qWzsXvieWG_siA/edit?utm_content=DAFyXmbskLU&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
         rel="noopener noreferrer"
         target="_blank"
-        className="absolute top-[25px] -right-10 flex text-xs items-center animate-revealTextAfter3250ms lg:top-24 md:top-[70px] md:-right-12 md:text-base sm:text-sm sm:top-[40px]"
+        className="absolute top-[25px] -right-10 flex text-xs items-center animate-revealTextAfter2750ms lg:top-24 md:top-[70px] md:-right-12 md:text-base sm:text-sm sm:top-[40px]"
       >
         (<GoArrowUpRight />
         Resume)
--- src/components/mainBanner/elements/Stack.tsx
+++ src/components/mainBanner/elements/Stack.tsx
@@ -4,19 +4,19 @@ import { BsArrowRightShort } from "react-icons/bs";
 function Stack() {
   return (
     <div className="relative flex z-20">
-      <div className="text-4xl animate-slideToLeftAfter2750ms lg:text-9xl md:text-8xl sm:text-6xl">
+      <div className="text-4xl animate-slideToLeftAfter2250ms lg:text-9xl md:text-8xl sm:text-6xl">
         -Stack
       </div>
-      <div className="absolute top-0 -right-20 flex flex-col justify-center text-xs lg:text-xl lg:top-7 lg:-right-40 md:text-lg md:top-3 md:-right-32 sm:text-sm sm:top-1">
-        <div className="flex items-center animate-revealTextAfter3000ms">
+      <div className="absolute top-0 -right-[100px] flex flex-col justify-center text-xs lg:text-xl lg:top-7 lg:-right-[196px] md:text-lg md:top-3 md:-right-40 sm:text-sm sm:top-1">
+        <div className="flex items-center animate-revealTextAfter2500ms">
           <BsArrowRightShort />
           <span>Optimized UX/UI</span>
         </div>
-        <div className="flex items-center animate-revealTextAfter3100ms">
+        <div className="flex items-center animate-revealTextAfter2600ms">
           <BsArrowRightShort />
           <span>Performance Tuning</span>
         </div>
-        <div className="flex items-center animate-revealTextAfter3200ms">
+        <div className="flex items-center animate-revealTextAfter2700ms">
           <BsArrowRightShort />
           <span>E2E Project Management</span>
         </div>
